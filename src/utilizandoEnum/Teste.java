564ad55c4e22ea8033a443791755792f57b59f8b
TadeuViegas <tadeucosta@acad.ifma.edu.br>
2023-03-20 15:00:44 -0300

    update files Enum

--- src/utilizandoEnum/Teste.java
+++ src/utilizandoEnum/Teste.java
@@ -8,16 +8,19 @@ public class Teste {
         Prioridade prioridadeFull = Prioridade.FULL;
 
         System.out.println(prioridadeMax.name());
+        System.out.println(prioridadeFull.name());
 
+        System.out.println(" ------ valores das constantes do Enum -----");
         System.out.println(prioridadeMin.ordinal());
         System.out.println(prioridadeNormal.ordinal());
         System.out.println(prioridadeMax.ordinal());
         System.out.println(prioridadeFull.ordinal());
 
-        System.out.println(" ------ novos valores das constantes do Enum -----");
+        System.out.println(" ------ novos valores (setados) nas constantes -----");
         System.out.println(prioridadeMin.getNumero());
         System.out.println(prioridadeNormal.getNumero());
         System.out.println(prioridadeMax.getNumero());
+        System.out.println(prioridadeFull.getNumero());
 
     }
 }
