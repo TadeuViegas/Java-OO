f7bd3d44f612ade6917bc97cae5f94395a59d18b
TadeuViegas <tadeucosta@acad.ifma.edu.br>
2023-03-20 14:22:12 -0300

    update files Generics

--- src/usandoGenerics/TesteGenerics.java
+++ src/usandoGenerics/TesteGenerics.java
@@ -2,9 +2,14 @@ package usandoGenerics;
 
 public class TesteGenerics {
     public static void main(String[] args) {
+//        Ao usar a classe com Generics eu posso declarar qual o tipo
         Generics<Boolean> g = new Generics<>();
+        Generics<Integer> g1 = new Generics<>();
 
-        g.addValue(2 > 1);
+        g.addValue(false);
         System.out.println(g.getValue());
+
+        g1.addValue(10);
+        System.out.println(g1.getValue());
     }
 }
