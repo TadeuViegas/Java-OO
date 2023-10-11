863656e360be4384b2abe1a2ed3767da80c50def
Tadeu Viegas <tadeu.viegas@matera.com>
2023-10-10 23:26:57 -0300

    Testes composição

--- src/clientesComposicao/TestaComposicaoObjetos.java
+++ src/clientesComposicao/TestaComposicaoObjetos.java
@@ -2,20 +2,23 @@ package clientesComposicao;
 
 public class TestaComposicaoObjetos {
     public static void main(String[] args) {
-        Cliente tadeu = new Cliente();
-        tadeu.nome = "Tadeu Viégas Dev";
-        tadeu.cpf = "04676079311";
-        tadeu.profissao = "developer";
+        Cliente cliente1 = new Cliente();
+        cliente1.nome = "Tadeu Viégas Dev";
+        cliente1.cpf = "04676079311";
+        cliente1.profissao = "developer";
 
-        Conta accTadeu = new Conta(tadeu,123,12345);
+        Conta conta1 = new Conta(cliente1,123,12345);
+//        accTadeu.cliente = cliente1;
 
-//        accTadeu.cliente = tadeu;
-        System.out.println(accTadeu.cliente);
-        System.out.println(accTadeu.agencia);
-        System.out.println(accTadeu.numero);
-        System.out.println(tadeu);
+        System.out.println(conta1.cliente);
+        System.out.println(conta1.agencia);
+        System.out.println(conta1.numero);
+        System.out.println(cliente1);
 
-        System.out.println(accTadeu.cliente.nome);
+        System.out.println(conta1.cliente.nome);
+        System.out.println(conta1.cliente.profissao);
+
+        System.out.println(conta1.toString());
 
     }
 }
--- src/teste/Main.java
+++ src/teste/Main.java
@@ -4,7 +4,7 @@ package teste;
 public class Main {
     public static void main(String[] args) {
 
-}
+    }
 }
 
 
