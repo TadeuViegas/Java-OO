d0c73d342c431835f5da64b9c6473b80b8960506
Tadeu Viegas <tadeu.viegas@matera.com>
2024-01-10 21:35:53 -0300

    test methods streams

--- src/streams/Streams.java
+++ src/streams/Streams.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-//Em java usamos Streams para se trabalhar com processamento de dados (collections, array, arquivos, regex)
+//Em java usamos Streams para se trabalhar com processamento de dados (collections, array, arquivos)
 // geralmente uma lista é convertida pra stream e assim pode-se usar metodos para tratar os dados da coleção
 public class Streams {
 
@@ -15,7 +15,9 @@ public class Streams {
         List<String> lista = Arrays.asList("a", "b", "c");
         List<String> result = lista.stream() // converte uma lista para uma stream
             .filter(l -> l.startsWith("a")) // operações intermediarias (filtram ou transformam(map) os dados):
-            .map(String::toUpperCase) // filter, map, sorted, peek
+            .peek(l -> System.out.println("valor filtrado:" + l))
+            .map(String::toUpperCase)
+            .sorted()
             .collect(Collectors.toList()); // operações terminal (que produzem resultado final): collect,
 
         System.out.println(result);
